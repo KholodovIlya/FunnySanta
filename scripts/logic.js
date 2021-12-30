9f77dac6c1f838665da29074ed9852b089089c21
KholodovIlya <96822606+KholodovIlya@users.noreply.github.com>
2021-12-30 14:29:02 +0300

    Update logic.js

--- scripts/logic.js
+++ scripts/logic.js
@@ -52,7 +52,7 @@ function render() {
 
 async function mainLoop() {
   const startTime = performance.now();
-  await update(); await collisions(); await render();
+  update(); await collisions(); await render();
   let sleepTime = fpsInterval - Math.abs(startTime - performance.now()); if (sleepTime < 0) sleepTime = 0;
   setTimeout(mainLoop, sleepTime);
 }
